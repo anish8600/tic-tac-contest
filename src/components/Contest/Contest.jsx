f4a592c06a578d029af1988e0c85c5820253f678
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 03:50:52 -0400

    logic to update and revert state

--- src/components/Contest/Contest.jsx
+++ src/components/Contest/Contest.jsx
@@ -5,8 +5,15 @@ import Steps from '../Steps/Steps';
 import { TIC_TAC_SOLUTION, TIC_TAC_CONSTANTS } from '../../helper/tictacHelper';
 import './Contest.css';
 
-function getSolution() {
-    console.log('get solution');
+function getSolution(baseArray){
+  const answers = TIC_TAC_SOLUTION;
+  for(let i=0; i<answers.length; i++){
+    const [a,b,c] = answers[i];
+    if(baseArray[a] && baseArray[a] === baseArray[b] && baseArray[a] === baseArray[c]){
+      return baseArray[a];
+    }
+  }
+  return null;
 }
 
 class Contest extends React.Component {
@@ -19,25 +26,45 @@ class Contest extends React.Component {
     };
   }
 
-  updateState = () => {
-    console.log('updateState operation');
+  updateState = (e) => {
+    const eventValue = e.target.value;
+    const chances = this.state.chances.slice(0, this.state.steps + 1);
+    const live = chances[chances.length - 1];
+    const baseArray = live.baseArray.slice();
+    if (getSolution(baseArray) || baseArray[eventValue]) {
+      return;
+    }
+    baseArray[i] = this.state.nextX ? TIC_TAC_CONSTANTS.X_SYMBOL : TIC_TAC_CONSTANTS.O_SYMBOL;
+    this.setState({
+      chances: chances.concat([{baseArray: baseArray}]),
+      steps: chances.length,
+      nextX: !this.state.nextX
+    });
   }
 
-  revertState = () => {
-    console.log('revertState operation');
+  revertState = (e) => {
+    const step = e.target.value;
+    this.setState({
+      steps: step,
+      nextX: (step % 2) === 0
+    });
   }
 
   render = () => {
+    const chances = this.state.chances;
+    const live = chances[this.state.steps];
+    const champion = getSolution(live.baseArray);
+    
     return (
       <div className="contest-container">
         <div className="contest-base">
           <h1> {TIC_TAC_CONSTANTS.TIC_TAC_HEADING} </h1>
-          <Base baseArray={this.state.chances} onClick={this.updateState} />
+          <Base baseArray={live.baseArray} onClick={this.updateState} />
         </div>
         <div className="contest-section">
-          <Turn champion={'X'} turn={this.state.nextX} steps={this.state.steps}/>
+          <Turn champion={champion} turn={this.state.nextX} steps={this.state.steps}/>
           <ul>
-            <Steps chances={this.state.chances} onClick={this.revertState}/>
+            <Steps chances={chances} onClick={this.revertState}/>
           </ul>
         </div>
       </div>
