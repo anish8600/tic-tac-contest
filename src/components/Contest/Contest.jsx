68e489ea3a62e0fd71668794a3b091906df29070
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-10 23:11:04 -0400

    Creating skeleton of Contest component as per design

--- src/components/Contest/Contest.jsx
+++ src/components/Contest/Contest.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import './Contest.css';
+
+function getSolution() {
+    console.log('get solution');
+}
 
 class Contest extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      chances: [],
+      steps: 0,
+      nextX: true
+    };
+  }
+
+  updateState = () => {
+    console.log('updateState operation');
+  }
+
+  revertState = () => {
+    console.log('revertState operation');
+  }
+
   render = () => {
     return (
       <div className="contest-base" />
