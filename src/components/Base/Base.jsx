dea6771eb386db044ad934106467520250904874
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 03:54:39 -0400

    logic to form matrix of sqaure

--- src/components/Base/Base.jsx
+++ src/components/Base/Base.jsx
@@ -2,13 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TicTac from '../TicTac/TicTac';
 
-const Base = (props) => {
+class Base extends React.Component{
+  formMatrix(i){
     return (
-      <div className="base-container">
-        <TicTac value={props.baseArray[0]} onClick={props.onClick}/>
-      </div>
+      <TicTac value={this.props.baseArray[i]} onClick={() => this.props.onClick(i)}/>
     );
-};
+  }
+  
+  render() {
+      return (
+        <div>
+          <div className="base">
+            {this.formMatrix(0)}
+            {this.formMatrix(1)}
+            {this.formMatrix(2)}
+          </div>
+          <div className="base">
+            {this.formMatrix(3)}
+            {this.formMatrix(4)}
+            {this.formMatrix(5)}
+          </div>
+          <div className="base">
+            {this.formMatrix(6)}
+            {this.formMatrix(7)}
+            {this.formMatrix(8)}
+          </div>
+        </div>
+      );
+    }
+}
 
 Base.propTypes = { baseArray: PropTypes.array.isRequired, onClick: PropTypes.func.isRequired};
 
