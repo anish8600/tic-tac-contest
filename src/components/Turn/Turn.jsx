2bc275060f691864d81c8260926acf3ef7f0a9fe
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 02:47:11 -0400

    creating turn component

---
+++ src/components/Turn/Turn.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Turn = (props) => <h2>Message</h2>
+
+export default Turn;
--- src/components/Turn/Turn.test.js
+++ src/components/Turn/Turn.test.js
@@ -6,7 +6,7 @@ describe('Turn', () => {
   let wrapper;
   beforeEach(() => wrapper = shallow(<Turn />));
 
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it('should render a <h2 />', () => {
+    expect(wrapper.find('h2').length).toEqual(1);
   });
 });
