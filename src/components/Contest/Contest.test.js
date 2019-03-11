8e97ae9e18ad19e0e8ea4ec92fb529caf8c38d7c
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-10 23:56:16 -0400

    type error in contest component

--- src/components/Contest/Contest.test.js
+++ src/components/Contest/Contest.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Contest from './Contest/Contest';
+import Contest from './Contest';
 
 describe('Contest', () => {
   let wrapper;
