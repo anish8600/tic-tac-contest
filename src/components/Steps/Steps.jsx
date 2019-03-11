5159e2f9b2e265b821cd2c97b6f9b00e2671577f
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 04:00:20 -0400

    logic for calculating steps

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TIC_TAC_CONSTANTS } from '../../helper/tictacHelper';
+import './Steps.css';
 
-const Steps = () => <li></li>
+const Steps = (props) => {
+    const turns = props.chances.map((step, turn) => {
+        const stepDesc = turn ? TIC_TAC_CONSTANTS.BACK_TO_STEP + turn : TIC_TAC_CONSTANTS.RESET_GAME;
+        return (
+            <li key={turn}>
+                <button className="step" onClick={() => props.onClick(turn)}>{stepDesc}</button>
+            </li>
+        );
+    });
+
+    return turns;
+}
+
 
 Steps.propTypes = { chances: PropTypes.array.isRequired,  onClick: PropTypes.func.isRequired };
 
