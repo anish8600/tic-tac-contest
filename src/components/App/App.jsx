38f1e3cc197912720739b7c30c806109628d60c8
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-10 23:08:22 -0400

    creating Contest component - All test cases passed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import Contest from '../Contest/Contest';
 import './App.css';
 
-const App = () => <div className="contest-container" />;
+const App = () => <div className="contest-container"><Contest /></div>;
 
 export default App;
---
+++ src/components/Contest/Contest.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Contest extends React.Component {
+  render = () => {
+    return (
+      <div className="contest-base" />
+    );
+  }
+}
+
+export default Contest;
