ba224fbcf2ebb3b3c424b401a0c3b7e5a44891f3
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-10 22:42:56 -0400

    integrating App component with index file and rendering it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App/App';
 import './index.css';
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById("tic-tac-container"));
+ReactDOM.render(<App />, document.getElementById("tic-tac-container"));
