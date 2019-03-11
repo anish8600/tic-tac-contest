9dc7dace41d15241040aad3d8b60b3dc2adbbd83
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 03:38:05 -0400

    snapshot test for Turn component

--- src/components/Turn/Turn.test.js
+++ src/components/Turn/Turn.test.js
@@ -6,6 +6,10 @@ describe('Turn', () => {
   let wrapper;
   beforeEach(() => wrapper = shallow(<Turn champion={'X'} turn={'X'} steps={3}/>));
 
+  it('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+  
   it('should render a <h2 />', () => {
     expect(wrapper.find('h2').length).toEqual(1);
   });
