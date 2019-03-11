0be604b12db6b37b0fd2a380c621ad88699be18a
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-10 22:50:22 -0400

    failing test case - App component rendering Contest component

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Contest from '../Contest/Contest';
 
 describe('App', () => {
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<App />));
+
   it('should render a <div />', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render the Contest Component', () => {
+    expect(wrapper.containsMatchingElement(<Contest />)).toEqual(true);
   });
 });
