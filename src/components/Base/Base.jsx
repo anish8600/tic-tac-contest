dc250cb496208ba327c1b698dc91b65713f72d6f
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 01:52:11 -0400

    base should contain tictac component - test case

--- src/components/Base/Base.jsx
+++ src/components/Base/Base.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TicTac from '../TicTac/TicTac';
 
-const Base = (props) => <div className="base-container" />;
+const Base = (props) =>{
+    return (
+      <div className="base-container">
+        <TicTac/>
+      </div>
+    );
+};
 
 Base.propTypes = { baseArray: PropTypes.array.isRequired, onClick: PropTypes.func.isRequired};
 
--- src/components/Base/Base.test.js
+++ src/components/Base/Base.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Base from './Base';
+import TicTac from '../TicTac/TicTac';
 
 describe('Contest', () => {
   let wrapper;
@@ -9,4 +10,9 @@ describe('Contest', () => {
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
+
+  it('should render the TicTac Component', () => {
+    expect(wrapper.containsMatchingElement(<TicTac />)).toEqual(true);
+  });
+
 });
