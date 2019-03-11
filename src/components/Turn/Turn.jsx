b0c84f9ad7026bdc9f8b015c78e57a1137b9cb2d
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 02:56:54 -0400

    Test case for prop types of turn component

--- src/components/Turn/Turn.jsx
+++ src/components/Turn/Turn.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Turn = (props) => <h2>Message</h2>
+
+Turn.propTypes = { champion: PropTypes.string.isRequired, 
+                   turn: PropTypes.string.isRequired,
+                   steps: PropTypes.number.isRequired
+                 };
 
 export default Turn;
--- src/components/Turn/Turn.test.js
+++ src/components/Turn/Turn.test.js
@@ -4,7 +4,7 @@ import Turn from './Turn';
 
 describe('Turn', () => {
   let wrapper;
-  beforeEach(() => wrapper = shallow(<Turn />));
+  beforeEach(() => wrapper = shallow(<Turn champion={'X'} turn={'X'} steps={3}/>));
 
   it('should render a <h2 />', () => {
     expect(wrapper.find('h2').length).toEqual(1);
