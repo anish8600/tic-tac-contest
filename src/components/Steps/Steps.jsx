c27c86109876a5dc75efabf86bd45d85e393c272
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 03:16:53 -0400

    test case passed- Prop types of component

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Steps = () => <li></li>
+
+Turn.propTypes = { chances: PropTypes.array.isRequired,  onClick: PropTypes.func.isRequired };
 
 export default Steps;
