b30fb752e334ed96eea4340a90d9937a59d6b0cb
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 03:16:22 -0400

    failing test for prop types of Steps

--- src/components/Steps/Steps.test.js
+++ src/components/Steps/Steps.test.js
@@ -4,7 +4,7 @@ import Steps from './Steps';
 
 describe('Steps', () => {
   let wrapper;
-  beforeEach(() => wrapper = shallow(<Steps />));
+  beforeEach(() => wrapper = shallow(<Steps chances={[]} onClick={jest.fn()} />));
 
   it('should render a <li />', () => {
     expect(wrapper.find('li').length).toEqual(1);
