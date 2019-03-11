42abac6f8378e243c02e4bd31f1c90dda3ed35ed
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 03:35:10 -0400

    test cases for scenarios of messages

--- src/components/Turn/Turn.jsx
+++ src/components/Turn/Turn.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { TIC_TAC_CONSTANTS, MAX_ATTEMPTS } from '../../helper/tictacHelper';
 import PropTypes from 'prop-types';
 
-const Turn = (props) => <h2>Message</h2>
+const Turn = (props) => {
+    let turnIndicator;
+    if (props.champion) {
+      turnIndicator = TIC_TAC_CONSTANTS.WINNER_MSG_1 + props.champion + TIC_TAC_CONSTANTS.WINNER_MSG_2;
+    } else if(props.steps < MAX_ATTEMPTS){
+      turnIndicator = TIC_TAC_CONSTANTS.TURN_MSG + (props.turn ? TIC_TAC_CONSTANTS.X_SYMBOL : TIC_TAC_CONSTANTS.O_SYMBOL);
+    } else{
+      turnIndicator = TIC_TAC_CONSTANTS.TIE_MSG;
+    }
+    return <h2>{turnIndicator}</h2>;
+}
 
 Turn.propTypes = { champion: PropTypes.string.isRequired, 
                    turn: PropTypes.string.isRequired,
--- src/components/Turn/Turn.test.js
+++ src/components/Turn/Turn.test.js
@@ -9,4 +9,18 @@ describe('Turn', () => {
   it('should render a <h2 />', () => {
     expect(wrapper.find('h2').length).toEqual(1);
   });
+
+  it('should display correct message for winner>', () => {
+    expect(wrapper.find('h2').text()).toEqual('Congratulations X.You are a Winner!!!!!!!!');
+  });
+
+  it('should display correct message for winner>', () => {
+    wrapper.setProps({ champion:null })
+    expect(wrapper.find('h2').text()).toEqual('Your turn: X');
+  });
+
+  it('should display correct message for tie scenario>', () => {
+    wrapper.setProps({ champion:null, steps:10 })
+    expect(wrapper.find('h2').text()).toEqual('There is tie. Click \'Reset Game\' to play again.');
+  });
 });
