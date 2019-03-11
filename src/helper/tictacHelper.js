b7aa13c86f222f02c2a82ad8af4fc0c1682d22bb
Daakshi Mahajan <45431522+daakshiM@users.noreply.github.com>
2019-03-11 03:24:52 -0400

    adding helper file

---
+++ src/helper/tictacHelper.js
@@ -0,0 +1,24 @@
+export const TIC_TAC_CONSTANTS = {
+    TIC_TAC_HEADING: 'TIC TAC TOE CONTEST',
+    X_SYMBOL: 'X',
+    O_SYMBOL: 'O',
+    BACK_TO_STEP: 'Back to Step #',
+    RESET_GAME: 'Reset Game',
+    WINNER_MSG_1:'Congratulations ',
+    WINNER_MSG_2:'.You are a Winner!!!!!!!!',
+    TURN_MSG:'Your turn: ',
+    TIE_MSG:'There is tie. Click \'Reset Game\' to play again.'
+}
+
+export const TIC_TAC_SOLUTION = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+export const MAX_ATTEMPTS = 9;
